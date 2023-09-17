d82c5a8d53079f1640718beaa1a70a01323eea80
jeremyrayjewell <jeremyrayjewell@gmail.com>
2023-09-16 22:27:52 -0400

    New game elements

--- runninggame/script.js
+++ runninggame/script.js
@@ -18,6 +18,11 @@ var checkDead = setInterval(function(){
     if(blockLeft<20 && blockLeft>0 && characterTop>=130){
         block.style.animation = "none";
         block.style.display = "none";
-        alert("You lose.");
+        alert("You lost.");
+        location.reload(); 
     }
 },10)
+
+document.addEventListener('keydown', function(event) {
+    jump();
+  });
