fa0f023680b63fcb1c54e1cac937d8d7439d2a3a
jeremyrayjewell <jeremyrayjewell@gmail.com>
2023-10-16 16:39:10 -0300

    additional

---
+++ tetris2/script.js
@@ -0,0 +1,99 @@
+class Tetris {
+    constructor(imageX, imageY, template) {
+        this.imageY = this.imageY
+        this.imageX = this.imageX
+        this.template = template
+    }
+
+    checkBottom() {}
+
+    checkLeft() {}
+
+    checkRight() {}
+
+    moveRight() {}
+
+    moveLeft() {}
+
+    moveBottom() {}
+    changeRotation() {}
+}
+
+const imageSquareSize = 24
+const size = 40
+const framePerSecond = 24
+const gameSpeed = 5
+const canvas = document.getElementById("canvas");
+const image = document.getElementById("image");
+const ctx = canvas.getContext("2d");
+const squareCountX = canvas.width / size;
+const squareCountY = canvas.height / size;
+
+const shapes = [
+    new Tetris(0, 120, [
+      [0, 1, 0],
+      [0, 1, 0],
+      [1, 1, 0],
+    ]),
+    new Tetris(0, 96, [
+      [0, 0, 0],
+      [1, 1, 1],
+      [0, 1, 0],
+    ]),
+    new Tetris(0, 72, [
+      [0, 1, 0],
+      [0, 1, 0],
+      [0, 1, 1],
+    ]),
+    new Tetris(0, 48, [
+      [0, 0, 0],
+      [0, 1, 1],
+      [1, 1, 0],
+    ]),
+    new Tetris(0, 24, [
+      [0, 0, 1, 0],
+      [0, 0, 1, 0],
+      [0, 0, 1, 0],
+      [0, 0, 1, 0],
+    ]),
+    new Tetris(0, 0, [
+      [1, 1],
+      [1, 1],
+    ]),
+  
+    new Tetris(0, 48, [
+      [0, 0, 0],
+      [1, 1, 0],
+      [0, 1, 1],
+    ]),
+  ];
+
+
+  let gameMap;
+  let gameOver;
+  let currentShape;
+  let nextShape;
+  let score;
+  let initialTwoDArr;
+
+  let gameLoop = () =< {
+    setInterval(update, 1000/ gameSpeed)
+    setInterval(draw, 1000/framePerSecond)
+  }
+
+  let update = () => {};
+
+  let draw = () => {};
+
+  let resetVars = () => {
+    initialTwoDArr = []
+    for(let i = 0; i < squareCountY; i++) {
+        for(let j = 0; j < squareCountX; j++ ){
+
+        }
+    }
+  }
+
+  gameLoop()
+
+  //11:19
