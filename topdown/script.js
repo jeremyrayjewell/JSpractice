f65b1263826defeb3a2398f963c7cfc4d5f252ba
jeremyrayjewell <jeremyrayjewell@gmail.com>
2023-10-31 20:56:58 -0300

    Consolidated js files

--- topdown/script.js
+++ topdown/script.js
@@ -1,8 +1,6 @@
 var character = document.querySelector(".character");
 var map = document.querySelector(".map");
 
-$.getScript("yul1.js");
-
 //Change char 
 
 $(document).ready(function(){
@@ -14,11 +12,10 @@ $(document).ready(function(){
        if (!toggle) {
            $(".character_spritesheet").css("background-image", "url('jer1.png')");
            $(this).text("Yulia");
-           $.getScript("jer1.js");
        } else {
            $(".character_spritesheet").css("background-image", "url('yul1.png')");
            $(this).text("Jeremy");
-           $.getScript("yul1.js");
+           
        }
        toggle = !toggle;
    });
@@ -92,24 +89,109 @@ step(); //kick off the first step!
 
 
 /* Direction key state */
-const directions = {
-   up: "up",
-   down: "down",
-   left: "left",
-   right: "right",
-}
-const keys = {
-   38: directions.up,
-   37: directions.left,
-   39: directions.right,
-   40: directions.down,
-}
-document.addEventListener("keydown", (e) => {
-   var dir = keys[e.which];
-   if (dir && held_directions.indexOf(dir) === -1) {
-      held_directions.unshift(dir)
+var canJump = true; // Initially, the character can jump
+var isCharacterInAir = false; // Flag to track if the character is in the air
+
+$(document).keydown(function(e) {
+    if (e.which == 32) { // If the key pressed was spacebar
+        e.preventDefault(); // Prevent the default action in all cases
+        if (characterIsOnGround()) {
+            makeCharacterJump();
+        }
+    }
+});
+
+$(document).keydown(function(e) {
+    if (isCharacterInAir) {
+        e.preventDefault(); // Prevent left and right movement while jumping
+    }
+});
+
+function getValueBasedOnBackgroundImage() {
+   const element = document.querySelector('.character_spritesheet');
+   const backgroundImage = element.style.backgroundImage;
+
+   if (backgroundImage.includes('jer1.png')) {
+       return {jumpHeight: 16, jumpDuration: 500};
+   } else {
+       return {jumpHeight: 22, jumpDuration: 1100}; // Default values
    }
-})
+}
+
+function makeCharacterJump() {
+   if (canJump) {
+       canJump = false; // Disable jumping
+       isCharacterInAir = true; // Character is in the air
+
+       var character = $(".character_spritesheet");
+       var originalTop = parseInt(character.css("top"));
+
+       // Get the jump height and duration based on the background image
+       var values = getValueBasedOnBackgroundImage();
+       var jumpHeight = values.jumpHeight;
+       var jumpDuration = values.jumpDuration;
+        // Move the character up with an ease-in-out animation
+        character.animate(
+            { top: (originalTop - jumpHeight) + 'px' },
+            {
+                duration: jumpDuration / 2,
+                easing: 'easeOutQuad',
+                complete: function () {
+                    // Move the character down with an ease-in-out animation
+                    character.animate(
+                        { top: originalTop + 'px' },
+                        {
+                            duration: jumpDuration / 2,
+                            easing: 'easeInQuad',
+                            complete: function () {
+                                canJump = true; // Enable jumping again after the jump is complete
+                                isCharacterInAir = false; // Character is back on the ground
+                            }
+                        }
+                    );
+                }
+            }
+        );
+    }
+}
+
+$(document).keydown(function(e) {
+    if (e.which == 32 && characterIsOnGround()) {
+        e.preventDefault(); // Prevent the default action
+        makeCharacterJump();
+    }
+});
+
+function characterIsOnGround() {
+    var character = $(".character_spritesheet");
+    var characterTop = parseInt(character.css("top"));
+    var groundLevel = 0; // Change this value to match your ground level
+
+    // Check if the character's top position is equal to or greater than the ground level
+    return characterTop >= groundLevel;
+}
+
+const directions = {
+    up: "up",
+    down: "down",
+    left: "left",
+    right: "right",
+ }
+ const keys = {
+    38: directions.up,
+    37: directions.left,
+    39: directions.right,
+    40: directions.down,
+ }
+ document.addEventListener("keydown", (e) => {
+     var dir = keys[e.which];
+     if (dir && held_directions.indexOf(dir) === -1) {
+         // Only allow movement if character is on the ground
+         if (characterIsOnGround()) {
+             held_directions.unshift(dir);
+         }
+     }
+ });
 
 document.addEventListener("keyup", (e) => {
    var dir = keys[e.which];
@@ -118,6 +200,7 @@ document.addEventListener("keyup", (e) => {
       held_directions.splice(index, 1)
    }
 });
+
 
 
 
@@ -167,10 +250,3 @@ document.querySelector(".dpad-left").addEventListener("mouseover", (e) => handle
 document.querySelector(".dpad-up").addEventListener("mouseover", (e) => handleDpadPress(directions.up));
 document.querySelector(".dpad-right").addEventListener("mouseover", (e) => handleDpadPress(directions.right));
 document.querySelector(".dpad-down").addEventListener("mouseover", (e) => handleDpadPress(directions.down));
-
-
-
-
-
-
-
