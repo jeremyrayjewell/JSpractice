f6f7c13a8ac85cae475d047091d4cf3231358e76
jeremyrayjewell <jeremyrayjewell@gmail.com>
2023-11-10 09:56:48 -0300

    card sprites

--- cardselector/script.js
+++ cardselector/script.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const cardContainer = document.getElementById("cardContainer");
-    const card = document.getElementById("card");
-    const cardBack = document.getElementById("cardBack");
-    const cardSpritesheet = document.getElementById("cardSpritesheet");
+// document.addEventListener("DOMContentLoaded", function() {
+//     const cardContainer = document.getElementById("cardContainer");
+//     const card = document.getElementById("card");
+//     const cardBack = document.getElementById("cardBack");
+//     const cardSpritesheet = document.getElementById("cardSpritesheet");
 
-    cardContainer.addEventListener("click", function() {
-        if (cardBack.style.display === "none") {
-            // Card is showing the front side
-            cardBack.style.display = "block";
-            cardSpritesheet.style.display = "none";
-        } else {
-            // Card is showing the back side
-            cardBack.style.display = "none";
-            cardSpritesheet.style.display = "block";
+//     cardContainer.addEventListener("click", function() {
+//         if (cardBack.style.display === "none") {
+//             // Card is showing the front side
+//             cardBack.style.display = "block";
+//             cardSpritesheet.style.display = "none";
+//         } else {
+//             // Card is showing the back side
+//             cardBack.style.display = "none";
+//             cardSpritesheet.style.display = "block";
 
-            // Change the background position to show a different part of the spritesheet
-            cardSpritesheet.style.backgroundPosition = "0 0"; // Adjust this according to your spritesheet layout
-        }
-    });
-});
+//             // Change the background position to show a different part of the spritesheet
+//             cardSpritesheet.style.backgroundPosition = "0 0"; // Adjust this according to your spritesheet layout
+//         }
+//     });
+// });
